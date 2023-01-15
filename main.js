d0b49ec161726b2eda8d54d3d9a77aa7e84336bb
Anto Alaminos <alamanton@tuta.io>
2023-01-15 19:03:06 +0100

    wrapping code into an object with methods

--- main.js
+++ main.js
@@ -1,27 +1,106 @@
+const FLTyping = {
+    currentLanguageCode : 'fr', //fr language by default
+    currentWord : '',
+    userInput : '',
+    currentList : [],
+    score : 0,
+
+    stringToArray: function(languageCode) {
+        let list = data[languageCode].split(', ');
+        return list;
+    },
+
+    shuffle: function(array) {
+        let shuffledList = array.sort((a, b) => 0.5 - Math.random());
+        return shuffledList;
+    },
+
+    startGame: function() {
+        //debugger;
+        if (this.currentWord == '') {
+            //if the game has not yet started or it is over, start
+        
+            //first we need a shuffled list
+            this.currentList = this.stringToArray(this.currentLanguageCode);
+            
+            currentList_shuffled = this.shuffle(this.currentList);
+            
+            //new word iterates along array
+            for (let i = 0; i < currentList_shuffled.length; i++) {
+                this.currentWord = currentList_shuffled[i];
+                this.userInput = prompt(this.currentWord);
+                if ( this.userInput != this.currentWord ) {
+                    console.log('WRONG!');
+                    currentList_shuffled.push(currentList_shuffled[i]);
+                    this.score -= 2;
+                        
+                } else {
+                    console.log('RIGHT!');
+                    ++this.score;
+                }
+            }
+            console.log('Your score = ' + this.score);
+            this.currentWord = '';
+            }
+            console.log('quieres seguir?');
+    }
+
+
+};
+
+const view = {
+    displayWord: function() {
+        //display word that user must type
+    },
+
+    displayScore: function() {
+        //display live score
+    },
+
+    setUpEventListeners: function() {
+        let app = document.getElementById('app');
+        
+        app.addEventListener('click', function(event) {
+            let element = event.target;
+        }
+        // wip
+
+    //eventListener for entering word
+    //eventlistener for language change
+        )
+    }
+};
+
+
 const data = {
     fr : "mysère, derrière, est-ce, être, règles, j'imagine, j'écoute, chèvre, ça, où, là, père, été, très content, même, pas encore, à Saint-Étienne, à Aix-en-Provence, à Nîmes, à Oléans, c'est le même, déjà, très grand, très petit, aprés nous, c'est déjà pas mal",
     ru : "уборная, открыто, Закрыто"
 };
 
+
+
+/*
 let currentLanguageCode = 'fr'; //fr language by default
 let currentWord = '';
 let userInput = '';
 let currentList = [];
 let score = 0;
-//let i = 0;
+
 
 function stringToArray(languageCode) {
     let list = data[languageCode].split(', ');
     return list;
 };
 
+
 //returns shuffled array
 function shuffle(array) {
     let shuffledList = array.sort((a, b) => 0.5 - Math.random());
     return shuffledList;
 };
+*/
 
-
+/*
 //play the array, then shuffle again if we reach the end
 function startGame() {
     //debugger;
@@ -51,15 +130,15 @@ function startGame() {
     currentWord = '';
     }
     console.log('quieres seguir');
-};
+};*/
 
 
-function FLTyping() {
+/*function FLTyping() {
     if ( userInput == currentWord ) {
         ++score;
         currentWord
     }
-}
+}*/
 
 /*
 function wordRandomPick(min,max) {
